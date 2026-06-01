c6639d56944ec5bb2865b265796aa7d1190e7575
walking-waffle <zzbrian603@gmail.com>
2026-06-01 23:34:01 +0800

    finish no bonus.

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@
 #include <sstream>
 #include <cstdlib>
 
-constexpr int MIN_FLOOR = 1;
-constexpr int MAX_FLOOR = 10;
-constexpr int MOVE_DELAY_MS = 1000;
+const int MIN_FLOOR = 1;
+const int MAX_FLOOR = 10;
+const int MOVE_DELAY_MS = 1000;
 
 void clear_screen()
 {
@@ -16,8 +16,9 @@ void clear_screen()
 #else
     std::system("clear");
 #endif
-}
+} // clear_screen
 
+class ElevatorSystem;
 class Elevator;
 void draw_shafts(const Elevator &e1, const Elevator &e2);
 
@@ -27,12 +28,14 @@ public:
     int current_floor;
     int target_floor;
     std::string name;
+    ElevatorSystem *system;
 
-    Elevator(std::string n, int floor)
+    Elevator(std::string n, int floor, ElevatorSystem *sys)
     {
         name = n;
         current_floor = floor;
         target_floor = 0;
+        system = sys;
     } // construct
 
     void display_floor() const
@@ -40,44 +43,7 @@ public:
         std::cout << "[" << name << "] Current floor: " << current_floor << "\n";
     } // display_floor
 
-    void move(int current, int floor, const Elevator &e1, const Elevator &e2)
-    {
-        current_floor = current;
-        target_floor = floor;
-
-        if (current == floor)
-        {
-            target_floor = 0;
-            clear_screen();
-            draw_shafts(e1, e2);
-            display_floor();
-            return;
-        } // if
-
-        int direction = (floor > current) ? 1 : -1;
-        int dist = std::abs(floor - current);
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(600));
-
-        for (int f = current + direction;; f += direction)
-        {
-            current_floor = f;
-            if (f == floor)
-                target_floor = 0;
-
-            clear_screen();
-            draw_shafts(e1, e2);
-            display_floor();
-
-            if (f == floor)
-                break;
-
-            std::this_thread::sleep_for(std::chrono::milliseconds(MOVE_DELAY_MS));
-        } // for
-
-        std::cout << "[" << name << "] Arrived at floor " << floor << "\n\n";
-        std::this_thread::sleep_for(std::chrono::milliseconds(800));
-    } // move
+    void move(int current, int floor);
 };
 
 std::string cell(const Elevator &e, int f)
@@ -110,62 +76,120 @@ void draw_shafts(const Elevator &e1, const Elevator &e2)
     std::cout << "  +------------+------------+\n";
 } // draw_shafts
 
-int get_int(const std::string &prompt)
+int get_int(const std::string &prompt, bool allow_zero = false)
 {
     int val;
+
     while (true)
     {
         std::cout << prompt;
         std::string line;
         std::getline(std::cin, line);
         std::istringstream iss(line);
-        iss >> val;
-        if (val == 0)
+        if (!(iss >> val))
+        {
+            std::cout << "Please enter a valid number.\n";
+            continue;
+        }
+        if (allow_zero && val == 0)
             return 0;
         if (val >= MIN_FLOOR && val <= MAX_FLOOR)
             return val;
-        std::cout << "  Please enter a number between "
-                  << MIN_FLOOR << " and " << MAX_FLOOR << ".\n";
+        std::cout << "Please enter a number between "
+                  << MIN_FLOOR << " and "
+                  << MAX_FLOOR << ".\n";
     } // while
-} // get_int
+}
 
-void run()
+class ElevatorSystem
 {
-    Elevator elevator1("Elevator1", 1);
-    Elevator elevator2("Elevator2", 1);
+    Elevator elevator1;
+    Elevator elevator2;
 
-    while (true)
+public:
+    ElevatorSystem()
+        : elevator1("Elevator1", 1, this), elevator2("Elevator2", 1, this) {}
+
+    void draw() const
     {
         clear_screen();
         draw_shafts(elevator1, elevator2);
+    } // draw
 
-        std::cout << "Enter 0 to exit.\n";
-        int from = get_int("  Your current floor (0-10): ");
-
-        if (from == 0)
-        {
-            std::cout << "\nFinish!\n\n";
-            break;
-        } // if
-
+    void dispatch(int from)
+    {
         int dist1 = std::abs(elevator1.current_floor - from);
         int dist2 = std::abs(elevator2.current_floor - from);
-
         Elevator &elev = (dist2 < dist1) ? elevator2 : elevator1;
 
         std::this_thread::sleep_for(std::chrono::milliseconds(800));
-
-        elev.move(elev.current_floor, from, elevator1, elevator2);
+        elev.move(elev.current_floor, from);
 
         int to = get_int("  Destination floor (1-10): ");
         std::cout << "\n";
 
-        elev.move(from, to, elevator1, elevator2);
-    } // while
-} // run
+        elev.move(from, to);
+    } // dispatch
+
+    void run()
+    {
+        while (true)
+        {
+            draw();
+
+            std::cout << "Enter 0 to exit.\n";
+            int from = get_int("Your current floor (0-10): ", true);
+
+            if (from == 0)
+            {
+                std::cout << "\nFinish!\n\n";
+                break;
+            } // if
+
+            dispatch(from);
+        } // while
+    } // run
+};
+
+void Elevator::move(int current, int floor)
+{
+    current_floor = current;
+    target_floor = floor;
+
+    if (current == floor)
+    {
+        target_floor = 0;
+        system->draw();
+        display_floor();
+        return;
+    } // if
+
+    int direction = (floor > current) ? 1 : -1;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(600));
+
+    for (int f = current + direction;; f += direction)
+    {
+        current_floor = f;
+        if (f == floor)
+            target_floor = 0;
+
+        system->draw();
+        display_floor();
+
+        if (f == floor)
+            break;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(MOVE_DELAY_MS));
+    } // for
+
+    std::cout << "[" << name << "] Arrived at floor " << floor << "\n\n";
+    std::this_thread::sleep_for(std::chrono::milliseconds(800));
+} // move
 
 int main()
 {
-    run();
+    ElevatorSystem system;
+    system.run();
     return 0;
 } // main
